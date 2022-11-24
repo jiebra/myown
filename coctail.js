84f788cb9a5a8ba84909d144d0d9ff84c05b9ac0
unknown <eliudgatiki9@gmail.com>
2022-11-25 00:42:17 +0300

    huh

---
+++ coctail.js
@@ -0,0 +1,7 @@
+function imgslider(anything){
+    document.querySelector('.starbucks').src =anything;
+}
+function changeCirleColor(color){
+    const circle= document.querySelector('.circle');
+    circle.style.background =color
+}
